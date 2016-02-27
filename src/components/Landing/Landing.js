fddbaa6b15a91ec8bdf5d8a22e8437a84b08b242
Robert Balderas <robert@rbdevelop.com>
2016-02-26 18:05:59 -0800

    folder setup

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Landing.scss';
+
+
+
+class ContactPage extends Component {
+
+
+}
