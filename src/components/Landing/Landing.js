9a98795c0d90ec420d7b91342da1b9d0640c0641
Robert Balderas <robert@rbdevelop.com>
2016-02-27 16:42:27 -0800

    better resolutions

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -19,7 +19,7 @@ class Landing extends Component {
         </div>
         <img
           className={s.apple}
-          src='../../AppleBadge.png'
+          src='../../app-store-badge.png'
         />
         <img
           className={s.phone}
