2b9d6257901c0d6f13ca087b4e06941fe8e642d9
Robert Balderas <robert@rbdevelop.com>
2016-03-03 09:41:12 -0600

    adding landing footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Footer.scss';
+import Link from '../Link';
+
+class Footer extends Component {
+
+}
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,9 +2,22 @@ import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Landing.scss';
 
+import Footer from '../Footer';*
+
 class Landing extends Component {
 
+  static contextTypes = {
+    onSetTitle: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  };
+
   render() {
+    this.context.onSetTitle('NAFC');
+
     return (
       <div className={s.landing}>
         <img
@@ -12,10 +25,9 @@ class Landing extends Component {
           src='../../Logo.png'
         />
         <div className={s.about}>
-          Let's help you send better messages to avoid a
-          fight, after all it's not a fight club! Type a message
-          get a sentiment score. Green is great, anything
-          else and you are on your own.
+          Let's help you send better messages to avoid a fight.
+          After all, it's not a fight club! Type a message and get a sentiment score.
+          Green is great… anything else, and you are on your own.
         </div>
         <img
           className={s.apple}
@@ -25,6 +37,7 @@ class Landing extends Component {
           className={s.phone}
           src='../../webphone@2x.png'
         />
+        <Footer />
       </div>
     );
   }
